2eb0241bd970454b56c27ace2b8384844a991f30
Никита Румянцев <rumiantcev.no@students.dvfu.ru>
2021-11-04 01:02:00 +1000

    6th commit

--- script.js
+++ script.js
@@ -1,65 +1,67 @@
+let time = new Date(0, 0, 0, 0, 0, 0, 0);
+let toTime = time;
+let timeout;
+showTime();
 
-let timer;
-let isStarted = false;
-function play()
-{
-    times = getTime();
-    console.log(times[0].value + " "+times[1].value + " "+ times[2].value);
-    countdown(times, timer);
-}
-
-function countdown(milliSecondsTime){
-    let el = document.querySelector('.timer');
-    timer = setInterval(function(){
-        milliSecondsTime = milliSecondsTime - 1000;
-        if(!isStarted) {
-            el.innerHTML = '';
-            clearTimeout(timer);
-        }
-        else {
-            if (milliSecondsTime / 1000 === 0) {
-                clearTimeout(timer);
-                el.innerHTML = 'BOOOOM';
-                isStarted = false;
-            } else {
-                el.innerHTML = showtime(milliSecondsTime);
-            }
-        }
-    },1000);
-}
-function showtime(time)
-{
-    let milliseconds = parseInt((time%1000))
-        , seconds = parseInt((time/1000)%60)
-        , minutes = parseInt((time/(1000*60))%60)
-        , hours = parseInt((time/(1000*60*60))%24);
-
-    hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
-
-    return hours + ":" + minutes + ":" + seconds;
-}
 
 function start()
 {
-    if (!isStarted) {
-        changeButton();
-        isStarted = true;
-        play();
-    }
-    else {
-        changeButton();
-        isStarted = false;
+    if (setTime())
+    {
+        showTime();
+        changeButton(false);
+        clearTimeout(timeout);
+        timer();
     }
 }
-function changeButton()
+
+async function timer(){
+    let promise = new Promise((resolve) => {
+        showTime()
+        if (toTime > time){
+            toTime.setSeconds(toTime.getSeconds()-1);
+            console.log(toTime.getSeconds);
+            timeout = setTimeout(timer, 1000);
+        }
+        else{
+            resolve("COPY PASTE");
+        }
+    })
+    promise.then(str => {
+        document.querySelector('.timer').innerHTML = str.toString()
+        changeButton(true);
+    });
+}
+
+
+function setTime()
+{
+    let selector = document.querySelectorAll('.selector_choose');
+    let sec = selector[0].value.split(' ')[0]
+    let min = selector[1].value.split(' ')[0]
+    let hour = selector[2].value.split(' ')[0]
+    if (Math.floor(sec + min + hour) !== 0)
+    {
+        toTime = new Date(0, 0,0, hour, min, sec,0);
+        return true;
+    }
+    else
+        return false;
+}
+
+function showTime()
+{
+    let el = document.querySelector('.timer');
+    el.innerHTML = toTime.toLocaleTimeString();
+}
+
+function changeButton(isStarted)
 {
     let button = document.querySelector(".play_button");
     if (!isStarted){
         button.style.border = "10px solid red";
         button.style.boxShadow = "0 0 5px red"
-        button.innerHTML = "stop"
+        button.innerHTML = "restart"
     }
     else {
         button.style.border = "10px solid greenyellow";
@@ -67,13 +69,3 @@ function changeButton()
         button.innerHTML = "start"
     }
 }
-
-function getTime()
-{
-    let selector = document.querySelectorAll('.selector_choose');
-    let seconds = selector[0].value;
-    let minutes = selector[1].value;
-    let hours= selector[2].value;
-}
-
-
